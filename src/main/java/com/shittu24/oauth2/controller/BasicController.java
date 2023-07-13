b6ed158e1068f098bb7b27166e2141e19f58d1ab
shittu24 <ibrahimshittu247@gmail.com>
2023-07-13 15:51:07 -0700

    main commit

---
+++ src/main/java/com/shittu24/oauth2/controller/BasicController.java
@@ -0,0 +1,16 @@
+package com.shittu24.oauth2.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo")
+public class BasicController {
+
+  @GetMapping
+  public ResponseEntity<String> sayHello() {
+    return ResponseEntity.ok("Hello OAuth2");
+  }
+}
